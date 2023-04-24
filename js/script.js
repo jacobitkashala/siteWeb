738c559660ca6b8af5d35714bd6eda7c8698feb8
JacobitKashala <Jacobitkashala@gmail.com>
2023-04-24 08:42:38 +0100

    refac

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let navbar = document.querySelector('.header .navbar');
-let searchForm = document.querySelector('.header .search-form');
-let loginForm = document.querySelector('.header .login-form');
-let contactInfo = document.querySelector('.contact-info');
+// let searchForm = document.querySelector('.header .search-form');
+// let loginForm = document.querySelector('.header .login-form');
+// let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
@@ -20,9 +20,9 @@ document.querySelector('#info-btn').onclick = () =>{
    contactInfo.classList.add('active');
 }*/
 
-document.querySelector('#close-contact-info').onclick = () =>{
-   contactInfo.classList.remove('active');
-}
+// document.querySelector('#close-contact-info').onclick = () =>{
+//    contactInfo.classList.remove('active');
+// }
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
