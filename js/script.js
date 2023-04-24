4782536fc3da463f230429bb2f23876ced16d66a
JacobitKashala <Jacobitkashala@gmail.com>
2023-04-24 08:30:56 +0100

    refac

--- js/script.js
+++ js/script.js
@@ -5,21 +5,20 @@ let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
 };
 
 
-
+/*
 document.querySelector('#login-btn').onclick = () =>{
    loginForm.classList.toggle('active');
    navbar.classList.remove('active');
    searchForm.classList.remove('active'); 
-};
+};*/
 
+/*
 document.querySelector('#info-btn').onclick = () =>{
    contactInfo.classList.add('active');
-}
+}*/
 
 document.querySelector('#close-contact-info').onclick = () =>{
    contactInfo.classList.remove('active');
@@ -27,9 +26,6 @@ document.querySelector('#close-contact-info').onclick = () =>{
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
-   contactInfo.classList.remove('active');
 }
 
 var swiper = new Swiper(".home-slider", {
