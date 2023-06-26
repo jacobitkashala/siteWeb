0d9c651317b5aec7858abee5492ba8fd32028dad
JacobitKashala <Jacobitkashala@gmail.com>
2023-06-26 18:10:58 +0100

    refactor:changer les images

---
+++ js/data.js
@@ -0,0 +1,19 @@
+const services =[
+        {titre:"Construction Métalique",description:"Construction De Métatique", img:"",order:1},
+        {titre:"Construction De Métatique",description:"Construction De Métatique", img:"",order:1},
+        {titre:"Rénovation Des Patrimoines",description:"Construction De Métatique", img:"",order:2},
+        {titre:"Rénovation Des Patrimoines",description:"Construction De Métatique", img:"",order:2},
+        {titre:"Rénovation Des Patrimoines",description:"Construction De Métatique", img:"",order:2},
+        {titre:"Rénovation Des Patrimoines",description:"Construction De Métatique", img:"",order:2},
+]
+
+
+const projets =[
+    {titre:"Maison de rêve",description:"construction, conception", img:"images/project-1.jpg",order:1},
+    {titre:"Maison de rêve",description:" design,construction", img:"images/project-2.jpg",order:2},
+    {titre:"Maison de rêve",description:" design,construction", img:"images/project-3.jpg",order:3},
+    {titre:"Maison de rêve",description:" design,construction", img:"images/project-4.jpg",order:4},
+    {titre:"Maison de rêve",description:" design,construction", img:"images/project-5.jpg",order:5},
+    {titre:"Maison de rêve",description:" design,construction", img:"images/project-6.jpg",order:6},
+]
+
